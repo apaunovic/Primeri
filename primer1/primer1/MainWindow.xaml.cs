672b770310b67469aba9b19cc66924a69c8b72c2
Aleksandar Paunovic <aleksandar.paunovic@gogridsmart.com>
2014-01-14 15:40:54 +0100

    XML commit

--- primer1/primer1/MainWindow.xaml.cs
+++ primer1/primer1/MainWindow.xaml.cs
@@ -15,8 +15,12 @@ using System.Windows.Shapes;
 using System.Net.NetworkInformation;
 using System.Net.Sockets;
 using System.Threading;
-using System.Threading.Tasks;
+//using System.Threading.Tasks;
 using primer1.Model;
+using System.Xml;
+using System.Xml.Linq;
+using System.IO;
+using System.Collections.ObjectModel;
 
 namespace primer1
 {
@@ -26,7 +30,7 @@ namespace primer1
     public partial class MainWindow : Window
     {
         private GasClient GasServerClient { get; set; }
-        private Task activeTask;
+        //private Task activeTask;
         CancellationTokenSource t;
         
         public MainWindow()
@@ -60,5 +64,52 @@ namespace primer1
         {
                 t.Cancel();             
          }
+
+        private void buttonMessage_Click(object sender, RoutedEventArgs e)
+        {
+            var errors = XElement.Load("C:/Users/Grid/ew.xml");
+
+            var eCount = errors.Elements().Where(el => "error".Equals(el.Attribute("type").Value)).Count();
+            var wCount = errors.Elements().Where(el => "warning".Equals(el.Attribute("type").Value)).Count();
+
+            readBox.Text=errors.ToString();
+
+            eBox.Text = " E:" + eCount + " -- W:" + wCount;
+
+           
+            
+        }
+
+        private void buttonErrors_Click(object sender, RoutedEventArgs e)
+        {
+            var errors = XElement.Load("C:/Users/Grid/Primeri/ew.xml");
+
+            /*var items = new List<string>();
+
+            foreach (XElement x in errors.Elements().Where(el => "error".Equals(el.Attribute("type").Value)))
+                {
+                    items.Add("Na liniji " + x.Attribute("line").Value.ToString() + " u fajlu " + x.Attribute("file").Value.ToString() + " postoji " + x.Attribute("text").Value.ToString());           
+                }
+    
+            lista.DataContext = items;*/
+
+            lista.DataContext = errors.Elements().Where(el => "error".Equals(el.Attribute("type").Value));
+        }
+
+        private void buttonWarnings_Click(object sender, RoutedEventArgs e)
+        {
+            var errors = XElement.Load("C:/Users/Grid/ew.xml");
+            
+            /*var items = new List<string>();
+
+            foreach (XElement x in errors.Elements().Where(el => "warning".Equals(el.Attribute("type").Value)))
+            {
+                items.Add("Na liniji " + x.Attribute("line").Value.ToString() + " u fajlu " + x.Attribute("file").Value.ToString() + " postoji " + x.Attribute("text").Value.ToString());
+            }
+
+            lista.DataContext = items;*/
+
+            lista.DataContext = errors.Elements().Where(el => "warning".Equals(el.Attribute("type").Value));
+        }
     }
 }
